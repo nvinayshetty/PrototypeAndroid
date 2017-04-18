943082be097dba3908b0042b7d94fc3da9706a6e
vinaya.n@redbus.com <vinaya.n@redbus.com>
2017-04-18 22:41:25 +0530

    Initial project import

---
+++ app/src/main/java/com/nvinayshetty/prototypeandroid/MainActivity.java
@@ -0,0 +1,15 @@
+package com.nvinayshetty.prototypeandroid;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+Button button;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
